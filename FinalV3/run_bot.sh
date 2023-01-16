bd284aed809018dcbebcc7cfc55539b996a71545
mos_at_laptop <mospakawat@gmail.com>
2023-01-16 22:37:24 +0700

    censored

--- FinalV3/run_bot.sh
+++ FinalV3/run_bot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # input:     id           passwd	 court  time
-bash bot.sh "6422782100" "1102200185194" "11" "17:00"
-# bash bot.sh "6422771756" "1830101157674" "2" "2" &
+bash bot.sh "#########" "##############" "##" "####"
+#bash bot.sh "id" "pass" "court" "time" &
