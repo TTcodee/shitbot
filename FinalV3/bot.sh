4c72235afa9d130c1f320ff1a87369ea470f9de0
mos_at_laptop <mospakawat@gmail.com>
2023-01-30 21:23:57 +0700

    fix bug when compute date

--- FinalV3/bot.sh
+++ FinalV3/bot.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-day=$(date "+%d")
-day=$[$day+2]
-month_and_year=$(date "+%m/%y")
-date=$(echo $day $month_and_year | awk '{printf("%s/%s"), $1, $2}')
-# run=$(find ~ -type f -name 'finalV2')
-run="python3 finalV3.py"
 
-# echo "User ID:"
-# read id
-# echo "Password:"
-# read pass
-# echo "row:"
-# read row
-# echo "time://17:00 = 1, 18:00 = 2, 19:00 = 3, 20:00 = 4"
-# read t
+date=$(date +"%d/%m/%y" --date="2 days")
+py_path=$(find ~ -name 'finalV3.py')
+run="python3 $py_path"
+
 id=$1
 pass=$2
 row=$3
